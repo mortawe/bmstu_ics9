1d397605cdd3e13054023e126d033aa06059dbb6
mortawe <mortawe@gmail.com>
2020-10-02 23:00:28 +0300

    Scripted auto-commit on change (2020-10-02 23:00:28) by gitwatch.sh

--- parallel_distributed_systems/lab1/src/main/java/WordMapper.java
+++ parallel_distributed_systems/lab1/src/main/java/WordMapper.java
@@ -9,11 +9,8 @@ public class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException,
             InterruptedException {
-        String line = value.toString();
-
-        line = line.toLowerCase();
-
-        String[] words = line.split("[^a-zA-Z]"); //не забыть русский текст через а-яА-Я
+        String[] words =
+                StringTools.split(StringTools.removeAllNonSymbols(line).toLowerCase());
 
         for (String word : words) {
             context.write(new Text(word), new IntWritable(1));
