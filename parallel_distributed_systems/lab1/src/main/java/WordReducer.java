7fbf600c50e049ce9f1b30339070301e7c2c4da4
mortawe <mortawe@gmail.com>
2020-10-02 22:22:22 +0300

    Scripted auto-commit on change (2020-10-02 22:22:22) by gitwatch.sh

--- parallel_distributed_systems/lab1/src/main/java/WordReducer.java
+++ parallel_distributed_systems/lab1/src/main/java/WordReducer.java
@@ -4,13 +4,15 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public class WordReducer extends Reducer<Text, IntWritable, Text, LongWritable> {
     @Override
     protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws
             IOException, InterruptedException {
+        Iterator it = values.iterator();
+        for (;it.hasNext();){
 
-//<считаем количество записей в итераторе и генерируем запись в контекст
-//        В контекст пишется пара — Text и LongWritable>
+        }
     }
 }
