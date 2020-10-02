a5b13e57cf282e4f34963b6e12e6f77d847d3e0e
mortawe <mortawe@gmail.com>
2020-10-02 21:34:25 +0300

    Scripted auto-commit on change (2020-10-02 21:34:25) by gitwatch.sh

--- parallel_distributed_systems/lab1/src/main/java/main.java
+++ parallel_distributed_systems/lab1/src/main/java/main.java
@@ -26,3 +26,12 @@ public class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 
     }
 }
+
+public class WordReducer extends Reducer<Text, IntWritable, Text, LongWritable> {
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws
+            IOException, InterruptedException {
+<считаем количество записей в итераторе и генерируем запись в контекст
+        В контекст пишется пара — Text и LongWritable>
+    }
+}
