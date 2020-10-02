a34d9280d2aca164eccac39b06352d7ef3a4d055
mortawe <mortawe@gmail.com>
2020-10-02 22:23:41 +0300

    Scripted auto-commit on change (2020-10-02 22:23:41) by gitwatch.sh

--- parallel_distributed_systems/lab1/src/main/java/WordReducer.java
+++ parallel_distributed_systems/lab1/src/main/java/WordReducer.java
@@ -11,8 +11,6 @@ public class WordReducer extends Reducer<Text, IntWritable, Text, LongWritable> 
     protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws
             IOException, InterruptedException {
         Iterator it = values.iterator();
-        for (; it.hasNext(); ) {
-
-        }
+        for (int count = 0; it.hasNext(); it.next(), count++) {}
     }
 }
