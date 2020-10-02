6d001254e47480ea78781a08715c41a1bd07b888
mortawe <mortawe@gmail.com>
2020-10-02 23:04:27 +0300

    Scripted auto-commit on change (2020-10-02 23:04:27) by gitwatch.sh

---
+++ parallel_distributed_systems/lab1/script.sh
@@ -0,0 +1,11 @@
+#bin/bash
+
+mvn package
+
+export HADOOP_CLASSPATH=target/hadoop-examples-1.0-SNAPSHOT.jar
+
+hadoop WordCountApp warandpeace1.txt output
+
+hadoop fs -copyToLocal output
+
+hadoop fs -rm -r output
