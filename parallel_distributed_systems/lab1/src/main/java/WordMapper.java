cf07f248369e9e59f7cb22b29ccb39208a849838
mortawe <mortawe@gmail.com>
2020-10-03 10:15:39 +0300

    Scripted auto-commit on change (2020-10-03 10:15:39) by gitwatch.sh

--- parallel_distributed_systems/lab1/src/main/java/WordMapper.java
+++ parallel_distributed_systems/lab1/src/main/java/WordMapper.java
@@ -2,6 +2,7 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+
 import java.io.IOException;
 
 public class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
