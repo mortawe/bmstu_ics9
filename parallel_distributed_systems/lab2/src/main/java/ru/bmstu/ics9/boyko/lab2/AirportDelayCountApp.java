d90aaf4a187cf5a40953358707aac5e48f86efbc
mortawe <mortawe@gmail.com>
2020-10-10 12:28:54 +0300

    Scripted auto-commit on change (2020-10-10 12:28:54) by gitwatch.sh

--- parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportDelayCountApp.java
+++ parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportDelayCountApp.java
@@ -1,5 +1,13 @@
 package ru.bmstu.ics9.boyko.lab2;
 
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
 public class AirportDelayCountApp {
     public static void main(String[] args) throws Exception {
         if (args.length != 2) {
