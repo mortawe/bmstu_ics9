18749e90c9baf27d6bf4101156f3e02311162393
mortawe <mortawe@gmail.com>
2020-10-10 12:28:31 +0300

    Scripted auto-commit on change (2020-10-10 12:28:31) by gitwatch.sh

--- parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportDelayCountApp.java
+++ parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportDelayCountApp.java
@@ -1,5 +1,21 @@
 package ru.bmstu.ics9.boyko.lab2;
 
 public class AirportDelayCountApp {
-    
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: WordCountApp <input path> <output path>");
+            System.exit(-1);
+        }
+        Job job = Job.getInstance();
+        job.setJarByClass(WordCountApp.class);
+        job.setJobName("Word count");
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        job.setMapperClass(WordMapper.class);
+        job.setReducerClass(WordReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setNumReduceTasks(2);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
 }
