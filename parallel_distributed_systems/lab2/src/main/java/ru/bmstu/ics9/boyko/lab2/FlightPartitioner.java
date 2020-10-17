22705ede01178b34546b08612ded8babec895dab
mortawe <mortawe@gmail.com>
2020-10-17 12:15:08 +0300

    Scripted auto-commit on change (2020-10-17 12:15:08) by gitwatch.sh

--- parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/FlightPartitioner.java
+++ parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/FlightPartitioner.java
@@ -1,4 +1,10 @@
 package ru.bmstu.ics9.boyko.lab2;
 
-public class FlightPartitioner {
+
+import org.apache.hadoop.mapreduce.Partitioner;
+
+public class FlightPartitioner <K, V> extends Partitioner<K, V> {
+    public int getPartition(K key, V value, int numReduceTasks) {
+        return (key.hashCode() & Integer.MAX_VALUE) % numReduceTasks;
+    }
 }
