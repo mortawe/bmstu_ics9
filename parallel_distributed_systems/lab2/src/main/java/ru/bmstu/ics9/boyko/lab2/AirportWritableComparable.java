a4f9ab652bdbce56fc15f0b38dbe659a996f52e5
mortawe <mortawe@gmail.com>
2020-10-17 11:13:39 +0300

    Scripted auto-commit on change (2020-10-17 11:13:39) by gitwatch.sh

--- parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportWritableComparable.java
+++ parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportWritableComparable.java
@@ -1,4 +1,25 @@
 package ru.bmstu.ics9.boyko.lab2;
 
-public class AirportWritableComparable {
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class AirportWritableComparable implements WritableComparable {
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }
