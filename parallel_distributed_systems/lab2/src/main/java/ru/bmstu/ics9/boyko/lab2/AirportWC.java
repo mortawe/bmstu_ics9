4f3e9d2a1b1af31c28565868cb12f18dd1f1e355
mortawe <mortawe@gmail.com>
2020-10-17 11:55:57 +0300

    Scripted auto-commit on change (2020-10-17 11:55:57) by gitwatch.sh

--- parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportWC.java
+++ parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportWC.java
@@ -18,7 +18,8 @@ public class AirportWC implements WritableComparable {
     public AirportWC(String value) {
         String[] rows = value.split(COMMA);
         this.dest_id = Integer.parseInt(rows[DEST_ROW_NUM]);
-        this.delay_time = Float.parseFloat(rows[DELAY_ROW_NUM]) > 0 ? Float.parseFloat(rows[DELAY_ROW_NUM]) : 0;
+        this.delay_time = Float.parseFloat(rows[DELAY_ROW_NUM]) > 0 ? Float.parseFloat(rows[DELAY_ROW_NUM]) : -1;
+//               negative delay is not delay at all
     }
 
     @Override
