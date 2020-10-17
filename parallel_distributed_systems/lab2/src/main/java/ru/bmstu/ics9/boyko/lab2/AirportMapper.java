97ebd606e0acee3965e4e1dee827ceee4c6e4bcc
mortawe <mortawe@gmail.com>
2020-10-17 09:53:41 +0300

    Scripted auto-commit on change (2020-10-17 09:53:41) by gitwatch.sh

--- parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportMapper.java
+++ parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportMapper.java
@@ -5,7 +5,12 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class AirportMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException,
+            InterruptedException {
 
 }
