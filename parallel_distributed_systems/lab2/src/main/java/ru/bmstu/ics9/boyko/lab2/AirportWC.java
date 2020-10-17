154c44bfac580e4f5cf2cea61f3ea633223b6fc9
mortawe <mortawe@gmail.com>
2020-10-17 12:01:50 +0300

    Scripted auto-commit on change (2020-10-17 12:01:50) by gitwatch.sh

--- parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportWC.java
+++ parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportWC.java
@@ -5,16 +5,18 @@ import org.apache.hadoop.io.WritableComparable;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.util.Objects;
 
 // :D
 public class AirportWC implements WritableComparable {
     public static final int DEST_ROW_NUM = 14;
     public static final int DELAY_ROW_NUM = 17;
-    public static final int CANCELLED_ROW = 
+    public static final int CANCELLED_ROW = 19;
     public static final String COMMA = ",";
 
     int dest_id;
     float delay_time;
+    boolean cancelled;
 
     public AirportWC(String value) {
         String[] rows = value.split(COMMA);
@@ -22,12 +24,27 @@ public class AirportWC implements WritableComparable {
         // negative delay is not delay at all
         // -1 is value for these flights
         this.delay_time = Float.parseFloat(rows[DELAY_ROW_NUM]) > 0 ? Float.parseFloat(rows[DELAY_ROW_NUM]) : -1;
-        if
+        this.cancelled = Boolean.parseBoolean(rows[CANCELLED_ROW]);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AirportWC airportWC = (AirportWC) o;
+        return dest_id == airportWC.dest_id &&
+                Float.compare(airportWC.delay_time, delay_time) == 0 &&
+                cancelled == airportWC.cancelled;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(dest_id, delay_time, cancelled);
     }
 
     @Override
     public int compareTo(Object o) {
-
+        return 0;
     }
 
     @Override
