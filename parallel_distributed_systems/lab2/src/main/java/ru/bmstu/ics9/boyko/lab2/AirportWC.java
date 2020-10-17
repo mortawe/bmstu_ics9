f327511f432a6be89ff05ca3b50364004fc49ac3
mortawe <mortawe@gmail.com>
2020-10-17 11:44:26 +0300

    Scripted auto-commit on change (2020-10-17 11:44:26) by gitwatch.sh

--- parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportWC.java
+++ parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportWC.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 // :D
 public class AirportWC implements WritableComparable {
     public static final int DEST_ROW_NUM = 14;
-    public static final int DELAY_ROW_NUM = 17;
+    public static final int DELAY_ROW_NUM = 16;
     public static final String COMMA = ",";
     int dest_id;
     float delay_time;
@@ -18,7 +18,7 @@ public class AirportWC implements WritableComparable {
         String[] rows = value.split(COMMA);
         this.dest_id = Integer.parseInt(rows[DEST_ROW_NUM]);
         this.delay_time = Float.parseFloat(rows[DELAY_ROW_NUM]);
-        
+
     }
 
     @Override
