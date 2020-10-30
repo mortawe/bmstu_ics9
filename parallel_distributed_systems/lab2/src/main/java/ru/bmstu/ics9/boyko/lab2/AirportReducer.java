e4ac783bf555d732286b00cfcb741758879e368d
mortawe <mortawe@gmail.com>
2020-10-30 23:41:47 +0300

    Scripted auto-commit on change (2020-10-30 23:41:47) by gitwatch.sh

--- parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportReducer.java
+++ parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportReducer.java
@@ -6,10 +6,24 @@ import org.apache.hadoop.mapreduce.Reducer;
 import java.io.IOException;
 
 public class AirportReducer extends Reducer<DelayWritableComparable, Text, Text, Text> {
+    private static final int DEFAULT_COUNTERS_VALUE = -1;
     @Override
     protected void reduce(DelayWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        float max = DEFAULT_COUNTERS_VALUE, min = DEFAULT_COUNTERS_VALUE;
+        boolean isFirstLine = true;
+        String airportName = "";
         for (Text value: values) {
-            
+            if (isFirstLine) {
+                airportName = value.toString();
+                continue;
+            }
+            float delay = Float.parseFloat(value.toString());
+            if (max == DEFAULT_COUNTERS_VALUE || max < delay) {
+                max = delay;
+            }
+            if (min == DEFAULT_COUNTERS_VALUE || min > delay) {
+                min = delay;
+            }
         }
     }
 }
