d94154d6418e504d1e1c1ddec636e33cf46160fd
mortawe <mortawe@gmail.com>
2020-10-30 22:01:43 +0300

    Scripted auto-commit on change (2020-10-30 22:01:43) by gitwatch.sh

--- parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/FlightWritable.java
+++ parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/FlightWritable.java
@@ -1,4 +1,21 @@
 package ru.bmstu.ics9.boyko.lab2;
 
-public class FlightWritable {
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class FlightWritable implements Writable {
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }
