b19a422954428ac275c6e9db125aa559d683e74c
mortawe <mortawe@gmail.com>
2020-10-30 23:17:07 +0300

    Scripted auto-commit on change (2020-10-30 23:17:07) by gitwatch.sh

--- parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/DelayWritableComparable.java
+++ parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/DelayWritableComparable.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 
 
-public class DelayWritableComparable implements WritableComparable {
+public class DelayWritableComparable implements WritableComparable <DelayWritableComparable> {
     public static final Integer STATE_AIRPORT = 0;
     public static final Integer STATE_FLIGHT = 1;
 
@@ -21,17 +21,23 @@ public class DelayWritableComparable implements WritableComparable {
     }
 
     @Override
-    public int compareTo(Object o) {
-        return 0;
+    public int compareTo(DelayWritableComparable o) {
+        // airport line first, flights second
+        if (airportID == o.airportID) {
+            return state - o.state;
+        }
+        return airportID - o.airportID;
     }
 
     @Override
     public void write(DataOutput dataOutput) throws IOException {
-
+        dataOutput.writeInt(airportID);
+        dataOutput.writeInt(state);
     }
 
     @Override
     public void readFields(DataInput dataInput) throws IOException {
-
+        airportID = dataInput.readInt();
+        state = dataInput.readInt();
     }
 }
