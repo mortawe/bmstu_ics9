07a213d49882b97a5b3d9c8ac4b8391472587704
mortawe <mortawe@gmail.com>
2020-10-30 22:40:56 +0300

    Scripted auto-commit on change (2020-10-30 22:40:56) by gitwatch.sh

--- parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportMapper.java
+++ parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportMapper.java
@@ -7,8 +7,13 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class AirportMapper extends Mapper<LongWritable, Text, DelayWritableComparable, Text> {
+    private static final String NEW_LINE = "\n";
+    private static final String COMMA = "";
+
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        
+        String[] lines = value.toString().split(NEW_LINE);
+
+
     }
 }
