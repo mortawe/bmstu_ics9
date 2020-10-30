58ba6cf02ade46377293e95c506bd304857df01c
mortawe <mortawe@gmail.com>
2020-10-30 23:22:45 +0300

    Scripted auto-commit on change (2020-10-30 23:22:45) by gitwatch.sh

--- parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportPartitioner.java
+++ parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportPartitioner.java
@@ -3,9 +3,9 @@ package ru.bmstu.ics9.boyko.lab2;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Partitioner;
 
-public class AirportPartitioner extends Partitioner <DelayWritableComparable, Text> {
+public class AirportPartitioner extends Partitioner<DelayWritableComparable, Text> {
     @Override
     public int getPartition(DelayWritableComparable delayWritableComparable, Text text, int numPartitions) {
-        return (delayWritableComparable.airportID & )% numPartitions);
+        return (delayWritableComparable.airportID & Integer.MAX_VALUE) % numPartitions;
     }
 }
