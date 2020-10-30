298b8ce3d565582d52ec6e76a5d6caf7fef9ba2f
mortawe <mortawe@gmail.com>
2020-10-30 23:00:16 +0300

    Scripted auto-commit on change (2020-10-30 23:00:16) by gitwatch.sh

--- parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportWritable.java
+++ parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportWritable.java
@@ -7,9 +7,12 @@ import java.io.DataOutput;
 import java.io.IOException;
 
 public class AirportWritable implements Writable {
+    private int code;
+    private String decription;
     @Override
     public void write(DataOutput dataOutput) throws IOException {
-
+        dataOutput.writeInt(code);
+        dataOutput.write
     }
 
     @Override
