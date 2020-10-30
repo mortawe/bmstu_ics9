6cfe71a63353344811ddd1d4a50e73fa7c9853c9
mortawe <mortawe@gmail.com>
2020-10-30 22:16:14 +0300

    Scripted auto-commit on change (2020-10-30 22:16:14) by gitwatch.sh

--- parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/FlightMapper.java
+++ parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/FlightMapper.java
@@ -1,5 +1,6 @@
 package ru.bmstu.ics9.boyko.lab2;
 
+import com.sun.org.apache.bcel.internal.generic.NEW;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -8,8 +9,10 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class FlightMapper extends Mapper <LongWritable, Text, DelayWritableComparable, IntWritable>{
+    private static final String NEW_LINE  = "\n";
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String data = value.toString()
+        String[] lines = value.toString().split(NEW_LINE);
+        
     }
 }
