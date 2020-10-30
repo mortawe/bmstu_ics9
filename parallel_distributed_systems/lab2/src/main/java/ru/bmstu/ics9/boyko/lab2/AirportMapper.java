22cd18104568e0c2a1d9b2e87cf3dbc016f7dc29
mortawe <mortawe@gmail.com>
2020-10-30 22:38:04 +0300

    Scripted auto-commit on change (2020-10-30 22:38:04) by gitwatch.sh

--- parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportMapper.java
+++ parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportMapper.java
@@ -4,5 +4,6 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class AirportMapper  extends Mapper<LongWritable, Text, DelayWritableComparable, Text> {
+public class AirportMapper extends Mapper<LongWritable, Text, DelayWritableComparable, Text> {
+
 }
