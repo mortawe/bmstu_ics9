0b3c26d8fcfd9b16667bfdecfabaa1d31575c528
mortawe <mortawe@gmail.com>
2020-10-30 23:31:00 +0300

    Scripted auto-commit on change (2020-10-30 23:31:00) by gitwatch.sh

--- parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportReducer.java
+++ parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportReducer.java
@@ -1,4 +1,6 @@
 package ru.bmstu.ics9.boyko.lab2;
 
-public class AirportReducer {
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class AirportReducer extends Reducer<DelayWritableComparable, Text, > {
 }
