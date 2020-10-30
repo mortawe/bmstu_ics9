ad6399ed4e6d28e469e8f989364968124d69d344
mortawe <mortawe@gmail.com>
2020-10-30 22:03:27 +0300

    Scripted auto-commit on change (2020-10-30 22:03:27) by gitwatch.sh

--- parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/WritableComparable.java
+++ parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/WritableComparable.java
@@ -1,6 +1,26 @@
 package ru.bmstu.ics9.boyko.lab2;
 
-public class WritableComparable extends org.apache.hadoop.io.WritableComparable < {
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class DelayWritableComparable implements WritableComparable {
 
 
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }
