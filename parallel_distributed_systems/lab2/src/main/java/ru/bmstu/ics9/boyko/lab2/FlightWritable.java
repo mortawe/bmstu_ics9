4e633b7a68cf937be3ac4abac3a3fb4b84bb8bdc
mortawe <mortawe@gmail.com>
2020-10-30 23:11:38 +0300

    Scripted auto-commit on change (2020-10-30 23:11:38) by gitwatch.sh

--- parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/FlightWritable.java
+++ parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/FlightWritable.java
@@ -7,14 +7,17 @@ import java.io.DataOutput;
 import java.io.IOException;
 
 public class FlightWritable implements Writable {
-    private int 
+    private int airportID;
+    private String delay;
     @Override
     public void write(DataOutput dataOutput) throws IOException {
-        dataOutput.
+        dataOutput.writeInt(airportID);
+        dataOutput.writeChars(delay);
     }
 
     @Override
     public void readFields(DataInput dataInput) throws IOException {
-
+        airportID = dataInput.readInt();
+        delay = dataInput.readLine();
     }
 }
