d32048f0170a70669e11a651612faa5d6a892c40
mortawe <mortawe@gmail.com>
2020-10-30 23:24:49 +0300

    Scripted auto-commit on change (2020-10-30 23:24:49) by gitwatch.sh

--- parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/DelayGroupingComparator.java
+++ parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/DelayGroupingComparator.java
@@ -3,4 +3,8 @@ package ru.bmstu.ics9.boyko.lab2;
 import org.apache.hadoop.io.RawComparator;
 
 public class DelayGroupingComparator implements RawComparator {
+    @Override
+    public int compare(byte[] bytes, int i, int i1, byte[] bytes1, int i2, int i3) {
+        return 0;
+    }
 }
