648d9d30a01e8c2d6c0ee3ed93509b162a46860d
mortawe <mortawe@gmail.com>
2020-10-30 23:31:55 +0300

    Scripted auto-commit on change (2020-10-30 23:31:55) by gitwatch.sh

--- parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportReducer.java
+++ parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportReducer.java
@@ -3,5 +3,11 @@ package ru.bmstu.ics9.boyko.lab2;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
+
 public class AirportReducer extends Reducer<DelayWritableComparable, Text, Text, Text> {
+    @Override
+    protected void reduce(DelayWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        super.reduce(key, values, context);
+    }
 }
