93048939934995f3cf3c67193c6c6537ed86c379
mortawe <mortawe@gmail.com>
2020-10-30 22:59:20 +0300

    Scripted auto-commit on change (2020-10-30 22:59:20) by gitwatch.sh

--- parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportWritable.java
+++ parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/AirportWritable.java
@@ -1,4 +1,19 @@
 package ru.bmstu.ics9.boyko.lab2;
 
-public class AirportWritable {
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class AirportWritable implements Writable {
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }
