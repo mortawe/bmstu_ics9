b47ba4910947d162d4527dd28621475a327e2620
mortawe <mortawe@gmail.com>
2020-10-30 22:10:27 +0300

    Scripted auto-commit on change (2020-10-30 22:10:27) by gitwatch.sh

--- parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/FlightMapper.java
+++ parallel_distributed_systems/lab2/src/main/java/ru/bmstu/ics9/boyko/lab2/FlightMapper.java
@@ -1,4 +1,12 @@
 package ru.bmstu.ics9.boyko.lab2;
 
-public class FlightMapper {
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class FlightMapper extends Mapper {
+    @Override
+    protected void map(Object key, Object value, Context context) throws IOException, InterruptedException {
+        super.map(key, value, context);
+    }
 }
