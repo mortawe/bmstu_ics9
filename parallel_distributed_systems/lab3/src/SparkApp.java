690bf5396c10249efbc919b9324b8fb2ccec2983
mortawe <mortawe@gmail.com>
2020-11-21 15:37:39 +0300

    Scripted auto-commit on change (2020-11-21 15:37:39) by gitwatch.sh

--- parallel_distributed_systems/lab3/src/SparkApp.java
+++ parallel_distributed_systems/lab3/src/SparkApp.java
@@ -1,4 +1,10 @@
+
+
 public class SparkApp {
     SparkConf conf = new SparkConf().setAppName("lab5");
     JavaSparkContext sc = new JavaSparkContext(conf);
+
+    JavaRDD<String> flightsFile = sc.textFile("664600583_T_ONTIME_sample.csv");
+    JavaRDD<String> airportsFile = sc.textFile("L_AIRPORT_ID.csv");
+
 }
