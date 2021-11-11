27efcec2f7fbd85922b6b61743ac18d642dd8057
mortawe <mortawe@gmail.com>
2021-11-12 00:51:32 +0300

    results

--- algebra-num-methods/lab4/test.go
+++ algebra-num-methods/lab4/test.go
@@ -2,7 +2,6 @@ package lab4
 
 import (
 	"fmt"
-	"log"
 	"math"
 	"math/rand"
 	"os"
@@ -12,7 +11,7 @@ import (
 	"gonum.org/v1/gonum/mat"
 
 	"algebra-num-methods/lab1"
-	"algebra-num-methods/lab3"
+	"algebra-num-methods/lab2/lib"
 )
 
 func eucDiffNorm(n int, m1, m2 []float64) float64 {
@@ -24,6 +23,14 @@ func eucDiffNorm(n int, m1, m2 []float64) float64 {
 	return norm
 }
 
+func eucNorm(n int, m2 []float64) float64 {
+	norm := 0.0
+	for i := 0; i < n; i++ {
+		norm += m2[i] * m2[i]
+	}
+	norm = math.Sqrt(norm)
+	return norm
+}
 func GenMatrix(n int) ([][]float64, []float64) {
 	m := make([][]float64, n)
 	b := make([]float64, n)
@@ -32,13 +39,13 @@ func GenMatrix(n int) ([][]float64, []float64) {
 	}
 	for i := 0; i < n; i++ {
 		for j := i + 1; j < n; j++ {
-			m[i][j] = float64(1000 + rand.Int()%1000)
+			m[i][j] = float64(rand.Int() % 100)  / 100000
 			m[j][i] = m[i][j]
 			// if i == j {
 			// 	m[i][i] = math.Abs(m[i][i]) * 100.0 * float64(n+1)
 			// }
 		}
-		b[i] = float64(1000 + rand.Int()%1000)
+		b[i] = rand.Float64()
 	}
 
 	for i := 0; i < n; i++ {
@@ -49,17 +56,71 @@ func GenMatrix(n int) ([][]float64, []float64) {
 			}
 			sum += math.Abs(m[i][j])
 		}
-		m[i][i] = sum * math.Abs(rand.Float64()) * 100000
+		m[i][i] = sum * 1.01
+		// m[i][i] =  float64(rand.Int() % 100)  / 100000
 	}
 	return m, b
 }
 
 func IterMethod1(n int, m [][]float64, b []float64, EPS float64) (int, []float64) {
+	err := 10.0
+	prev := make([]float64, n)
+	copy(prev, b)
+	steps := 0
+	// g := SubsMatrix(n, GenUnitMatrix(n), m)
+	for ; ; steps++ {
+		// fmt.Println(prev)
+		current := make([]float64, n)
+		for i := 0; i < n; i++ {
+			current[i] = prev[i] + b[i]
+			for j := 0; j < n; j++ {
+				current[i] += -m[i][j] * prev[j]
+			}
+		}
+		err = eucDiffNorm(n, current, prev)
+		if math.Abs(err)*mt <= EPS {
+			return steps, prev
+		}
+		if math.IsNaN(err) {
+			return -1, prev
+		}
+		prev = current
+	}
+	return steps, prev
+}
+func IterMethod2(n int, m [][]float64, b []float64, EPS float64) (int, []float64) {
+	err := 10.0
+	prev := make([]float64, n)
+	copy(prev, b)
+	steps := 0
+	// g := SubsMatrix(n, GenUnitMatrix(n), m)
+	for ; ; steps++ {
+		// fmt.Println(prev)
+		current := make([]float64, n)
+		for i := 0; i < n; i++ {
+			current[i] = b[i]
+			for j := 0; j < n; j++ {
+				current[i] += m[i][j] * prev[j]
+			}
+		}
+		err = eucDiffNorm(n, current, prev)
+		if math.Abs(err)*mt <= EPS {
+			return steps, prev
+		}
+		if math.IsNaN(err) {
+			return -1, prev
+		}
+		prev = current
+	}
+	return steps, prev
+}
+
+func IterMethod(n int, m [][]float64, b []float64, EPS float64) (int, []float64) {
 	err := 1.0
 	prev := make([]float64, n)
 	copy(prev, b)
 	steps := 0
-	for ; err >= EPS && steps < 1000; steps++ {
+	for ; err >= EPS; steps++ {
 		current := make([]float64, n)
 		for i := 0; i < n; i++ {
 			current[i] = b[i]
@@ -75,55 +136,6 @@ func IterMethod1(n int, m [][]float64, b []float64, EPS float64) (int, []float64
 		prev = current
 	}
 	return steps, prev
-}
-
-func IterMethod2(n int, m [][]float64, b []float64, EPS float64) (int, []float64) {
-	err := 1.0
-	prev := make([]float64, n)
-	copy(prev, b)
-	steps := 0
-	for ; err >= EPS; steps++ {
-		current := make([]float64, n)
-		copy(current, b)
-		for i := 0; i < n; i++ {
-			for j := 0; j < n; j++ {
-				current[i] += m[i][j] * prev[j]
-			}
-		}
-		err = eucDiffNorm(n, current, prev)
-		copy(prev, current)
-	}
-	return steps, prev
-}
-
-func IterMethod(n int, m [][]float64, b []float64, EPS float64) (int, []float64) {
-	err := 1.0
-	prev := make([]float64, n)
-	copy(prev, b)
-	steps := 0
-	for ; err >= EPS; steps++ {
-		current := make([]float64, n)
-		copy(current, prev)
-		for i := 0; i < n; i++ {
-			for j := 0; j < n; j++ {
-				current[i] += m[i][j] * prev[j]
-			}
-			current[i] -= b[i]
-		}
-		err = eucDiffNorm(n, current, prev)
-		copy(prev, current)
-	}
-	return steps, prev
-}
-
-func Flat(n int, x [][]float64) []float64 {
-	result := []float64{}
-	for j := 0; j < n; j++ {
-		for i := j; i < n; i++ {
-			result = append(result, x[j][i])
-		}
-	}
-	return result
 }
 
 func AddMatrix(n int, l, r [][]float64) [][]float64 {
@@ -159,60 +171,78 @@ func MultConstMatrix(n int, r [][]float64, c float64) [][]float64 {
 	return m
 }
 
+var mt = 0.0
+
 func GenUnitMatrix(n int) [][]float64 {
 	m := make([][]float64, n)
 	for i := 0; i < n; i++ {
 		m[i] = make([]float64, n)
 		for j := 0; j < n; j++ {
-			m[i][j] = 1
+			if i == j {
+				m[i][j] = 1
+			} else {
+				m[i][j] = 0
+			}
+
 		}
 	}
 	return m
 }
-
 func Test() {
-	// n := 30
-	// mGen, b := GenMatrix(n)
-	n, mGen, b := lab3.ReadMatrix("lab4/test/1")
+	n := 5
+	mGen, b := GenMatrix(n)
+	// n, mGen, b := lab3.ReadMatrix("lab4/test/1")
 	a := mat.NewSymDense(n, lab1.Flat(mGen))
-	det := mat.Det(a)
-	if det < 0 {
-		log.Fatal("det < 0")
-	}
+	fmt.Println(mGen)
 	var eigsym mat.EigenSym
 	_ = eigsym.Factorize(a, true)
 	eigs := eigsym.Values(nil)
 	tL, tR, tOpt := 0.0, 2/floats.Max(eigs), 2/(floats.Max(eigs)+floats.Min(eigs))
+
 	fmt.Println(tL, tR, tOpt)
 	step := (tR - tL) / 100
 	xs, ys, zs := []float64{}, []float64{}, []float64{}
-	_, res := IterMethod1(n, mGen, b, 0.001)
+	eps := 0.001
+	res, _ := lib.GaussPartial(mGen, b)
+	fmt.Println(res)
 
-	for i := tL + step; i <= tR; i += step {
-		m := SubsMatrix(n, GenUnitMatrix(n), MultConstMatrix(n, mGen, i))
+	// _, res1 := IterMethod(n, mGen, b, eps)
+
+	g := make([]float64, n)
+	for j := 0; j < n; j++ {
+		g[j] = b[j] * tOpt
+	}
+	// _, res1 := IterMethod(n,  MultConstMatrix(n, mGen, tOpt), g, eps)
+	// fmt.Println(res, res1)
+	for i := tL + step; i <= tR + step; i += step {
+	// for i := tOpt - step; i <= tR; i += step {
+		t := i
+		if tOpt-i < step {
+			t = tOpt
+		}
+		m := SubsMatrix(n, GenUnitMatrix(n), MultConstMatrix(n, mGen, t))
 		g := make([]float64, n)
 		for j := 0; j < n; j++ {
-			g[j] = -b[j] * i
+			g[j] = b[j] * t
 		}
-		// fmt.Println(steps1)
-
-		steps, res1 := IterMethod1(n, m, g, 0.001)
-		// fmt.Println(steps)
+		mDet := mat.NewSymDense(n, lab1.Flat(m))
+		mt = mat.Norm(mDet, 1)
+		fmt.Println(mt)
+		if mt > 1 {
+			// panic("norm > 1")
+		}
+		mt = math.Abs(mt / (1 - mt))
+		steps, res1 := IterMethod2(n, m, g, eps)
+		if steps == -1 {
+			continue
+		}
 		xs = append(xs, float64(steps))
-		ys = append(ys, i)
-		if math.IsNaN(eucDiffNorm(n, res, res1)) {
-			zs = append(zs, 0.0)
-		} else {
-			zs = append(zs, eucDiffNorm(n, res, res1))
-		}
+		ys = append(ys, t)
+		zs = append(zs, eucDiffNorm(n, res, res1))
 	}
-	fmt.Println(len(xs), len(ys), len(zs))
-	// m := AddMatrix(n, GenUnitMatrix(n), MultConstMatrix(n, mGen, tOpt))
-	// g := make([]float64, n)
-	// for j := 0; j < n; j++ {
-	// 	g[j] = -b[j] * tOpt
-	// }
-	// steps, _ := IterMethod1(n, m, g, 0.001)
+	fmt.Println(len(xs), floats.Max(xs), floats.Min(xs))
+	fmt.Println(len(ys), floats.Max(ys), floats.Min(ys))
+	fmt.Println(ys[floats.MinIdx(zs)], floats.Max(ys))
 
 	graph := chart.Chart{
 		Background: chart.Style{
@@ -234,26 +264,60 @@ func Test() {
 			},
 		},
 		Series: []chart.Series{
-			// chart.ContinuousSeries{
-			// Name:    "Params",
-			// XValues: ys,
-			// YValues: xs,
-		// },
 			chart.ContinuousSeries{
-				Name:    "Errors",
+				Name:    "Params",
 				XValues: ys,
-				YValues: zs,
-			// },
-			// chart.AnnotationSeries{
-			// 	Annotations: []chart.Value2{
-			// 		{XValue: tOpt, YValue: float64(steps), Label: "Optimal"}},
+				YValues: xs,
+			},
+			chart.AnnotationSeries{
+				Annotations: []chart.Value2{
+					{XValue: tOpt, YValue: xs[0], Label: "Optimal"}},
 			}},
 	}
 	graph.Elements = []chart.Renderable{
 		chart.LegendLeft(&graph),
 	}
 
-	f, _ := os.Create("duffing.png")
+	f, _ := os.Create("steps.png")
 	defer f.Close()
 	graph.Render(chart.PNG, f)
+
+	graphErr := chart.Chart{
+		Background: chart.Style{
+			Padding: chart.Box{
+				Top:  20,
+				Left: 100,
+			},
+		},
+		XAxis: chart.XAxis{
+			Name: "Param",
+			ValueFormatter: func(v interface{}) string {
+				return fmt.Sprintf("%v", v)
+			},
+		},
+		YAxis: chart.YAxis{
+			Name: "Error",
+			ValueFormatter: func(v interface{}) string {
+				return fmt.Sprintf("%v", v)
+			},
+		},
+		Series: []chart.Series{
+			chart.ContinuousSeries{
+				Name:    "Errors",
+				XValues: ys,
+				YValues: zs,
+			},
+			chart.AnnotationSeries{
+				Annotations: []chart.Value2{
+					{XValue: tOpt, YValue: zs[0], Label: "Optimal"}},
+			}},
+	}
+	graphErr.Elements = []chart.Renderable{
+		chart.LegendLeft(&graphErr),
+	}
+
+	fErr, _ := os.Create("errors.png")
+	defer fErr.Close()
+	graphErr.Render(chart.PNG, fErr)
+
 }
